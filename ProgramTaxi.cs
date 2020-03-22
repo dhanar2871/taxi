9a142186e1a3a01fa727500cb37f96beb0fa736f
dhanar2871 <dhanar.2871@students.amikom.ac.id>
2020-03-22 10:26:28 +0700

    taxi

---
+++ ProgramTaxi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LatihanClassDanObject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Taxi taxi = new Taxi();
+			taxi.DriverName = "supir taxi";
+			taxi.OnDuty = true;
+			taxi.NumPassenger = 10;
+
+			taxi.TaxiInfo();
+			taxi.PickupPassenger();
+			taxi.DropOffPassenger();
+
+			Console.ReadKey();
+		}
+
+	}
+}
